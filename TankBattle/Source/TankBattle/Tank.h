313968169c8773322251b07cd1982ed76688810f
Ben Aldridge <jeffgarcia2004@hotmail.com>
2017-09-30 15:58:38 -0700

    Partially implemented creating projectile on click (on Fire())

--- TankBattle/Source/TankBattle/Tank.h
+++ TankBattle/Source/TankBattle/Tank.h
@@ -10,6 +10,7 @@
 class UTankAimingComponent; 
 class UTankBarrel;
 class UTankTurret;
+class AProjectile;
 
 UCLASS()
 class TANKBATTLE_API ATank : public APawn
@@ -47,4 +48,7 @@ private:
 	
 	UPROPERTY(EditAnywhere, Category = Firing)
 		float LaunchSpeed = 5000;
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+		TSubclassOf<AProjectile> ProjectileBlueprint;
 };
